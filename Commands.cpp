9f9078a4c8805be9c6bf372194c24ec3506c72ec
menimen <meni.ashurov@gmail.com>
2019-12-24 01:31:56 +0200

    added DefineVar,Assaign,SendToSimulator,getFromSimulator Commands, temp save there is still work left to be done

--- Commands.cpp
+++ Commands.cpp
@@ -1,35 +1,54 @@
 //file to write all Commands implementation
 #include "Command.h"
 #include "Singleton.h"
-int OpenServerCommand :: execute(string str) {
+#include <string>
+int OpenServerCommand :: execute(string str[]) {
  return 0;
 }
 OpenServerCommand ::~OpenServerCommand() {
 }
 
-int ConnectCommand :: execute(string str) {
+int ConnectCommand :: execute(string str[]) {
   return 1;
 }
 
 ConnectCommand ::~ConnectCommand() {}
 
 
-int DefineVarCommand::execute(string str) {
-  Singleton *database=database->getInstance() ;
-  string key = database->getVector().at(0);
-  Var_Data temp(0,nullptr);
-  database->getsymbolTableToServerMap().emplace(key,temp);
-  return 1;
+int DefineVarCommand::execute(string str[]) {
+  Singleton* t = t->getInstance();
+  for(int i=0; i < 2;i++){
+    cout << (str[i]) << endl;
+  }
+  t->getCommandMap()["sim"]->execute(str);
+  return 6;
 }
-int SimCommand::execute(string str) {
-  return 1;
+int SimCommand::execute(string str[]) {
+  Singleton* t = t->getInstance();
+  if(str[1]=="->") {
+    t->getCommandMap()["->"]->execute(str);
+  } else if(str[1]=="<-") {
+    t->getCommandMap()["<-"]->execute(str);
+  }
+  return 0;
 }
-int setToClientCommand::execute(string str) {
-  return 1;
+int setToClientCommand::execute(string str[]) {
+  Singleton* t = t->getInstance();
+  Var_Data temp(0,str[3]);
+  t->getsymbolTableFromServerMap().emplace(str[1],temp);
+  return 0;
 }
-int setToSimulatorCommand::execute(string str) {
-  return 1;
+int setToSimulatorCommand::execute(string str[]) {
+  Singleton* t = t->getInstance();
+  Var_Data temp(0,str[1]);
+  t->getsymbolTableToServerMap().emplace(str[1],temp);
+  string c = to_string(1.05);
+  t->getArrayOfOrdersToServer().emplace(t->getArrayOfOrdersToServer().begin(), "set " + str[1] + c);
+  return 0;
 }
-int assignCommand::execute(string str) {
-  return 1;
+int assignCommand::execute(string str[]) {
+  Singleton* t = t->getInstance();
+  double val = stod(str[2]);
+  t->getsymbolTableToServerMap()["throttle"].set_value(val);
+  return 3;
 }
