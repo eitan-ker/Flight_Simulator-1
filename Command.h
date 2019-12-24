e87540d71d365bbdce7b6e13329b6eed998d656b
menimen <meni.ashurov@gmail.com>
2019-12-24 12:40:45 +0200

    modified Command.h to make execute method to accept string array like Eitan wanted

--- Command.h
+++ Command.h
@@ -2,54 +2,55 @@
 #define FLIGHTSIMULATOR__COMMAND_H_
 #include <string>
 #include "Var_Data.h"
+#include <vector>
 
 using namespace std;
 
 class Command {
  public:
-  virtual int execute(string str) = 0;
+  virtual int execute(string str[]) = 0;
   virtual ~Command()= default;
 };
 
 class ConnectCommand : public Command {
-  int execute(string str) override;
+  int execute(string str[]) override;
   ~ConnectCommand() override;
 };
 
 class OpenServerCommand: public Command {
-  int execute(string str) override;
+  int execute(string str[]) override;
   ~OpenServerCommand() override;
 };
 
 class DefineVarCommand : public Command {
  public:
-  int execute(string str);
+  virtual int execute(string str[]);
   void addTosymbolTable(string key, Var_Data value);
   ~DefineVarCommand(){};
 };
 
 class SimCommand : public Command {
  public:
-  int execute(string str);
-  ~SimCommand(){};
+  virtual int execute(string str[]);
+  virtual ~SimCommand(){};
 };
 
 class setToClientCommand : public Command {
  public:
-  int execute(string str);
+  virtual int execute(string str[]);
   ~setToClientCommand(){};
 };
 
 class setToSimulatorCommand : public Command {
  public:
-  int execute(string str);
-  ~setToSimulatorCommand(){};
+  virtual int execute(string str[]);
+  virtual ~setToSimulatorCommand(){};
 };
 
 class assignCommand : public Command {
  public:
-  int execute(string str);
-  ~assignCommand(){};
+  virtual int execute(string str[]);
+  virtual ~assignCommand(){};
 };
 
 
